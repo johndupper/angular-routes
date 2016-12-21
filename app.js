c5d7bd510fbb1f68d480ebb618f035645be75e0b
John Dupper <john.dupper@me.com>
2016-12-21 16:33:00 -0500

    made new html file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = angular.module('myApp', ['ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'home.html' //
+    });
+});
+
 app.controller('moviesCtrl', function() {
     this.title = 'Movies with Angular Routes';
 });
-
